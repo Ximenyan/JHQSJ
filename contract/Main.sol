ac93d1c517daf6bb4ae60db8731a2b59a1a9b489
wangwei <347364217@qq.com>
2020-12-22 19:48:41 +0800

    修改合约

--- contract/Main.sol
+++ contract/Main.sol
@@ -1,11 +1,18 @@
 // SPDX-License-Identifier: SimPL-2.0
 pragma solidity 0.7.4;
-contract JHQSJ {
+import "./access/Ownable.sol";
+contract JHQSJ is Ownable{
     mapping(address => mapping(string => uint256)) public uintdata;
     mapping(address => mapping(string => int256)) public intdata;
     mapping(address => mapping(string => bool)) public booldata;
     mapping(address => mapping(string => string)) public stringdata;
     mapping(string => address) router;//路由
     constructor(){
+    }
+}
+contract Main{
+    JHQSJ A;
+    constructor(){
+        A = new JHQSJ();
     }
 }
